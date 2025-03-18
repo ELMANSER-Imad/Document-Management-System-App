a8ed9ef50a253b08f55274683b9b2f437f672554
Imad Elmanser <elmanser.emad@gmail.com>
2025-03-18 03:31:18 +0000

    Initial commit: Ajout du projet Document Management System

---
+++ views/includes/footer.php
@@ -0,0 +1,18 @@
+
+</div>
+<!--===============================================================================================-->
+	<script src="views/includes/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="views/includes/vendor/bootstrap/js/popper.js"></script>
+	<script src="views/includes/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="views/includes/vendor/tilt/tilt.jquery.min.js"></script>
+<!--===============================================================================================-->
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+</body>
+</html>
